0f8cc19d24d28aa6c79f18373268d8ccae93d819
Alex Rea <git@area.io>
2015-10-02 11:23:13 +0100

    Add a reset method.
    Note that it waits until nothing is queued up; you could conceivably
    be waiting a long time for this to happen...
    
    Added as I want to run consecutive integration tests without
    restarting the server, just by resetting the database etc.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-var assign        = require('lodash.assign'),
-    bind          = require('lodash.bind'),
-    isEmpty       = require('lodash.isempty'),
-    jsondiffpatch = require('jsondiffpatch'),
+var assign = require('lodash.assign'),
+  bind = require('lodash.bind'),
+  isEmpty = require('lodash.isempty'),
+  jsondiffpatch = require('jsondiffpatch'),
 
-    COMMANDS  = require('./commands'),
-    utils     = require('./utils'),
-    Server;
+  COMMANDS = require('./commands'),
+  utils = require('./utils'),
+  Server;
 
-Server = function(adapter, transport, diffOptions){
-  if(!(adapter && transport)){ throw new Error('Need to specify an adapter and a transport'); }
-  if(!diffOptions){ diffOptions = {}; }
+Server = function(adapter, transport, diffOptions) {
+  if (!(adapter && transport)) {
+    throw new Error('Need to specify an adapter and a transport');
+  }
+  if (!diffOptions) {
+    diffOptions = {};
+  }
 
   this.adapter = adapter;
   this.transport = transport;
@@ -24,7 +28,9 @@ Server = function(adapter, transport, diffOptions){
   // set up the jsondiffpatch options
   // see here for options: https://github.com/benjamine/jsondiffpatch#options
   diffOptions = assign({
-    objectHash: function(obj) { return obj.id || obj._id || JSON.stringify(obj); }
+    objectHash: function(obj) {
+      return obj.id || obj._id || JSON.stringify(obj);
+    }
   }, diffOptions);
 
   this.jsondiffpatch = jsondiffpatch.create(diffOptions);
@@ -36,7 +42,7 @@ Server = function(adapter, transport, diffOptions){
  * Registers the correct event listeners
  * @param  {Connection} connection The connection that should get tracked
  */
-Server.prototype.trackConnection = function(connection){
+Server.prototype.trackConnection = function(connection) {
   connection.on(COMMANDS.join, this.joinConnection.bind(this, connection));
   connection.on(COMMANDS.syncWithServer, this.receiveEdit.bind(this, connection));
 };
@@ -47,8 +53,8 @@ Server.prototype.trackConnection = function(connection){
  * @param  {String} room             room identifier
  * @param  {Function} initializeClient Callback that is being used for initialization of the client
  */
-Server.prototype.joinConnection = function(connection, room, initializeClient){
-  this.getData(room, connection.userid, function(error, data){
+Server.prototype.joinConnection = function(connection, room, initializeClient) {
+  this.getData(room, connection.userid, function(error, data) {
     // connect to the room
     connection.join(room);
 
@@ -74,25 +80,51 @@ Server.prototype.joinConnection = function(connection, room, initializeClient){
 };
 
 /**
+ * Reset all queues / cached data
+ * @param  {Function} callback The function that should be called when complete
+ */
+Server.prototype.reset = function(callback) {
+  var waitForDiffSyncServer = function() {
+    var pending = false;
+    for (room in this.saveRequests) {
+      if (this.saveRequests[room] === true) {
+        pending = true;
+      }
+    }
+    if (pending) {
+      setTimeout(waitForDiffSyncServer, 1000);
+    } else {
+      this.data = {};
+      this.requests = {};
+      this.saveRequests = {};
+      this.saveQueue = {};
+      callback();
+    }
+  }.bind(this);
+  waitForDiffSyncServer();
+};
+
+/**
  * Gets data for a room from the internal cache or from the adapter
  * @param  {String}   room     room identifier
+ * @param  {String}   userid   The id of the user who made the requests
  * @param  {Function} callback notifier-callback
  */
-Server.prototype.getData = function(room, userid, callback){
+Server.prototype.getData = function(room, userid, callback) {
   var cachedVersion = this.data[room],
-      cache = this.data,
-      requests = this.requests;
+    cache = this.data,
+    requests = this.requests;
 
-  if(cachedVersion){
+  if (cachedVersion) {
     callback(null, cachedVersion);
-  }else{
+  } else {
     // if there is no request for this room
     // ask the adapter for the data
     // do nothing in the else case because this operation
     // should only happen once
-    if(!requests[room]){
+    if (!requests[room]) {
       requests[room] = true;
-      this.adapter.getData(room, userid, function(error, data){
+      this.adapter.getData(room, userid, function(error, data) {
         cache[room] = {
           registeredSockets: [],
           clientVersions: {},
@@ -102,7 +134,7 @@ Server.prototype.getData = function(room, userid, callback){
         requests[room] = false;
         callback(null, cache[room]);
       });
-    }else{
+    } else {
       requests[room] = true;
     }
   }
@@ -114,29 +146,29 @@ Server.prototype.getData = function(room, userid, callback){
  * @param  {Object} editMessage  The message containing all edits
  * @param  {Function} sendToClient The callback that sends the server changes back to the client
  */
-Server.prototype.receiveEdit = function(connection, editMessage, sendToClient){
+Server.prototype.receiveEdit = function(connection, editMessage, sendToClient) {
   // -1) The algorithm actually says we should use a checksum here, I don't think that's necessary
   // 0) get the relevant doc
-  this.getData(editMessage.room, connection.userid, function(err, doc){
+  this.getData(editMessage.room, connection.userid, function(err, doc) {
     // 0.a) get the client versions
     var clientDoc = doc.clientVersions[connection.id];
 
     // no client doc could be found, client needs to re-auth
-    if(err || !clientDoc){
+    if (err || !clientDoc) {
       connection.emit(COMMANDS.error, 'Need to re-connect!');
       return;
     }
 
     // when the versions match, remove old edits stack
-    if(editMessage.serverVersion === clientDoc.shadow.serverVersion){
+    if (editMessage.serverVersion === clientDoc.shadow.serverVersion) {
       clientDoc.edits = [];
     }
 
     // 1) iterate over all edits
-    editMessage.edits.forEach(function(edit){
+    editMessage.edits.forEach(function(edit) {
       // 2) check the version numbers
-      if(edit.serverVersion === clientDoc.shadow.serverVersion &&
-        edit.localVersion === clientDoc.shadow.localVersion){
+      if (edit.serverVersion === clientDoc.shadow.serverVersion &&
+        edit.localVersion === clientDoc.shadow.localVersion) {
         // versions match
         // backup! TODO: is this the right place to do that?
         clientDoc.backup.doc = utils.deepCopy(clientDoc.shadow.doc);
@@ -152,14 +184,14 @@ Server.prototype.receiveEdit = function(connection, editMessage, sendToClient){
         // doc.serverCopy = snapshot;
 
         // 3.a) increase the version number for the shadow if diff not empty
-        if(!isEmpty(edit.diff)){
+        if (!isEmpty(edit.diff)) {
           clientDoc.shadow.localVersion++;
         }
-      }else{
+      } else {
         // TODO: implement backup workflow
         // has a low priority since `packets are not lost` - but don't quote me on that :P
         console.log('error', 'patch rejected!!', edit.serverVersion, '->', clientDoc.shadow.serverVersion, ':',
-                    edit.localVersion, '->', clientDoc.shadow.localVersion);
+          edit.localVersion, '->', clientDoc.shadow.localVersion);
       }
     }.bind(this));
 
@@ -167,7 +199,7 @@ Server.prototype.receiveEdit = function(connection, editMessage, sendToClient){
     this.saveSnapshot(editMessage.room, connection.userid);
 
     // notify all sockets about the update, if not empty
-    if(editMessage.edits.length > 0){
+    if (editMessage.edits.length > 0) {
       this.transport.to(editMessage.room).emit(COMMANDS.remoteUpdateIncoming, connection.id);
     }
 
@@ -175,43 +207,43 @@ Server.prototype.receiveEdit = function(connection, editMessage, sendToClient){
   }.bind(this));
 };
 
-Server.prototype.saveSnapshot = function(room, userid){
+Server.prototype.saveSnapshot = function(room, userid) {
   var noRequestInProgress = !this.saveRequests[room],
-      checkQueueAndSaveAgain = function(){
-        // if another save request is in the queue, save again
-        var anotherRequestScheduled = this.saveQueue[room] === true;
-        this.saveRequests[room] = false;
-        if(anotherRequestScheduled){
-          this.saveQueue[room] = false;
-          this.saveSnapshot(room, userid);
-        }
-      }.bind(this);
+    checkQueueAndSaveAgain = function() {
+      // if another save request is in the queue, save again
+      var anotherRequestScheduled = this.saveQueue[room] === true;
+      this.saveRequests[room] = false;
+      if (anotherRequestScheduled) {
+        this.saveQueue[room] = false;
+        this.saveSnapshot(room, userid);
+      }
+    }.bind(this);
 
   // only save if no save going on at the moment
-  if(noRequestInProgress){
+  if (noRequestInProgress) {
     this.saveRequests[room] = true;
     // get data for saving
-    this.getData(room, userid, function(err, data){
+    this.getData(room, userid, function(err, data) {
       // store data
-      if(!err && data){
+      if (!err && data) {
         this.adapter.storeData(room, userid, data.serverCopy, checkQueueAndSaveAgain);
-      }else{
+      } else {
         checkQueueAndSaveAgain();
       }
     }.bind(this));
-  }else{
+  } else {
     // schedule a new save request
     this.saveQueue[room] = true;
   }
 };
 
-Server.prototype.sendServerChanges = function(doc, clientDoc, send){
+Server.prototype.sendServerChanges = function(doc, clientDoc, send) {
   // create a diff from the current server version to the client's shadow
   var diff = this.jsondiffpatch.diff(clientDoc.shadow.doc, doc.serverCopy);
   var basedOnServerVersion = clientDoc.shadow.serverVersion;
 
   // add the difference to the server's edit stack
-  if(!isEmpty(diff)){
+  if (!isEmpty(diff)) {
     clientDoc.edits.push({
       serverVersion: basedOnServerVersion,
       localVersion: clientDoc.shadow.localVersion,
